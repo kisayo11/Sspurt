5403b088ff23f928c6d6c6fd64956dde9231d566
kisayo11 <sangyong.kimm@gmail.com>
2024-09-25 17:51:28 +0900

    9/25 MyAccoutFragment 를 ㅅ실행하면 문제 가있는 상태로 커밋 ㅠㅠㅠ

--- app/src/main/java/com/kisayo/sspurt/fragments/HomeFragment.kt
+++ app/src/main/java/com/kisayo/sspurt/fragments/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.kisayo.sspurt.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kisayo.sspurt.activities.TrackingStartActivity
 import com.kisayo.sspurt.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,6 +24,10 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+       // 플로팅버튼 "Start" 클릭리스너
+        binding.startFab.setOnClickListener {
+            val intent = Intent(requireContext(), TrackingStartActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
