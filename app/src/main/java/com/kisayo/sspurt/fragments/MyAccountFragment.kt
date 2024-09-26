a723d61ba0728574a518a951a2ee14188d28b716
kisayo11 <sangyong.kimm@gmail.com>
2024-09-26 09:27:14 +0900

    9/26 마이어카운트프레그먼트 오류 수정. 쉐어드프레퍼런스 초기화 오류 수정완료 ㅠㅠ

--- app/src/main/java/com/kisayo/sspurt/fragments/MyAccountFragment.kt
+++ app/src/main/java/com/kisayo/sspurt/fragments/MyAccountFragment.kt
@@ -1,5 +1,6 @@
 package com.kisayo.sspurt.fragments
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Bitmap
@@ -42,6 +43,9 @@ class MyAccountFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // SharedPreferences 초기화
+        sharedPreferences = requireContext().getSharedPreferences("LoginInfo", Context.MODE_PRIVATE)
 
         // UserRepository 초기화
         userRepository = UserRepository(requireContext())
